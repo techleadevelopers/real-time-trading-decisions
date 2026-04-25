746952c496e162fbc337688db68cacc4e2f19ce8
devopscomputer <pauloti.engenharia@gmail.com>
2026-04-25 00:15:52 -0300

    chore: update rtts/src/main.rs

--- rtts/src/main.rs
+++ rtts/src/main.rs
@@ -1,11 +1,12 @@
 use anyhow::Result;
 use clap::Parser;
-use scalp_sniper_rtts::{config::Config, metrics::Metrics, pipeline};
+use scalp_sniper_rtts::{config::Config, metrics::Metrics, model_weights, pipeline};
 use tracing_subscriber::{fmt, EnvFilter};
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
     let cfg = Config::parse();
+    model_weights::initialize(&cfg.model_weights_path)?;
     let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     fmt().with_env_filter(filter).json().init();
 
